dbd7e8bb5a22e7ad2b9e2d1c1b79222602de7003
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2022-10-14 18:17:08 +0200

    Move relevant files to /src/ folder. Easier comparison.

---
+++ src/vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'bf3f72f77f1dfd8e94d7708e4890ab8917b84143',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bf3f72f77f1dfd8e94d7708e4890ab8917b84143',
+    ),
+    'matthiasmullie/minify' => 
+    array (
+      'pretty_version' => '1.3.69',
+      'version' => '1.3.69.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a61c949cccd086808063611ef9698eabe42ef22f',
+    ),
+    'matthiasmullie/path-converter' => 
+    array (
+      'pretty_version' => '1.1.3',
+      'version' => '1.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e7d13b2c7e2f2268e1424aaed02085518afa02d9',
+    ),
+  ),
+);
