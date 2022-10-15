052d16a58459ef069a8699453ae6eab61d18d3d6
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2022-10-16 00:36:08 +0200

    Create build.js

---
+++ build.js
@@ -0,0 +1,104 @@
+#!/usr/bin/env node
+const path = require('path');
+
+/* Configure START */
+const pathBuildKram = path.resolve("../buildKramGhsvs");
+const updateXml = `${pathBuildKram}/build/update.xml`;
+const changelogXml = `${pathBuildKram}/build/changelog.xml`;
+const releaseTxt = `${pathBuildKram}/build/release.txt`;
+/* Configure END */
+
+const replaceXml = require(`${pathBuildKram}/build/replaceXml.js`);
+const helper = require(`${pathBuildKram}/build/helper.js`);
+
+//const fse = require(`${pathBuildKram}/node_modules/fs-extra`);
+const pc = require(`${pathBuildKram}/node_modules/picocolors`);
+
+let {
+	name,
+	filename,
+	version,
+	versionSub
+} = require("./package.json");
+
+const manifestFileName = `${filename}.xml`;
+const Manifest = `${__dirname}/package/${manifestFileName}`;
+const source = `${__dirname}/node_modules/prismjs`;
+const target = `./media`;
+// let versionSub = '';
+
+let replaceXmlOptions = {
+	"xmlFile": "",
+	"zipFilename": "",
+	"checksum": "",
+	"dirname": __dirname,
+	"jsonString": "",
+	"versionSub": versionSub
+};
+let zipOptions = {};
+let from = "";
+let to = "";
+
+(async function exec()
+{
+	let cleanOuts = [
+		`./package`,
+		`./dist`,
+		"./_composer/vendor/bin",
+		"./_composer/vendor/matthiasmullie/minify/bin",
+		"./_composer/vendor/matthiasmullie/minify/.github"
+	];
+
+	await helper.cleanOut(cleanOuts);
+	await console.log(pc.cyan(pc.bold(`Be patient! Composer actions!`)));
+
+	from = `./_composer/vendor`;
+	to = `./package/vendor`;
+	await helper.copy(from, to)
+
+	from = `./src`;
+	to = `./package`;
+	await helper.copy(from, to)
+
+	await helper.mkdir('./dist');
+
+	const zipFilename = `${name}-${version}_${versionSub}.zip`;
+
+	replaceXmlOptions = {...replaceXmlOptions,
+		"xmlFile": Manifest,
+		"zipFilename": zipFilename
+	};
+
+	await replaceXml.main(replaceXmlOptions);
+	await helper.copy(`${Manifest}`, `./dist/${manifestFileName}`)
+
+	// Create zip file and detect checksum then.
+	const zipFilePath = path.resolve(`./dist/${zipFilename}`);
+
+	zipOptions = {
+		"source": path.resolve("package"),
+		"target": zipFilePath
+	};
+	await helper.zip(zipOptions)
+
+	replaceXmlOptions.checksum = await helper._getChecksum(zipFilePath);
+
+	// Bei diesen werden zuerst Vorlagen nach dist/ kopiert und dort erst "replaced".
+	for (const file of [updateXml, changelogXml, releaseTxt])
+	{
+		from = file;
+		to = `./dist/${path.win32.basename(file)}`;
+		await helper.copy(from, to)
+
+		replaceXmlOptions.xmlFile = path.resolve(to);
+		await replaceXml.main(replaceXmlOptions);
+	}
+
+	cleanOuts = [
+		`./package`
+	];
+	await helper.cleanOut(cleanOuts).then(
+		answer => console.log(pc.cyan(pc.bold(pc.bgRed(
+			`Finished. Good bye!`))))
+	);
+})();
